360c881e06a2ad7febed5c2a3ae5f910721aafa4
saadmarwan16 <saadmarwan16@gmail.com>
2024-02-06 23:14:41 +0300

    Factory reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import type { GetServerSideProps, NextPage } from "next";
-import ContactUs from "../src/shared/components/ContactUs";
-import Details from "../src/modules/home/components/Details";
-import Footer from "../src/shared/components/Footer";
-import HomeCampaigns from "../src/modules/home/components/HomeCampaigns";
-import HomeCategories from "../src/modules/home/components/HomeCategories";
-import HomeMainHero from "../src/modules/home/components/HomeMainHero";
-import MissionAndVision from "../src/modules/home/components/MissionAndVision";
-import SupportMethods from "../src/modules/home/components/SupportMethods";
-import homeController from "../src/modules/home/controllers/home_controller";
-import { HomeModel } from "../src/modules/home/data/models/home_page_model";
-import Meta from "../src/shared/components/Meta";
-import { ErrorModel } from "../src/shared/errors/error_model";
-import { BiErrorAlt } from "react-icons/bi";
-import AboutUs from "../src/modules/home/components/AboutUs";
-import Achievements from "../src/modules/home/components/Achievements";
-import { useAuthContext } from "../src/modules/auth/AuthContext";
+import type { GetServerSideProps, NextPage } from 'next';
+import ContactUs from '../src/shared/components/ContactUs';
+import Details from '../src/modules/home/components/Details';
+import Footer from '../src/shared/components/Footer';
+import HomeCampaigns from '../src/modules/home/components/HomeCampaigns';
+import HomeCategories from '../src/modules/home/components/HomeCategories';
+import HomeMainHero from '../src/modules/home/components/HomeMainHero';
+import MissionAndVision from '../src/modules/home/components/MissionAndVision';
+import SupportMethods from '../src/modules/home/components/SupportMethods';
+import homeController from '../src/modules/home/controllers/home_controller';
+import { HomeModel } from '../src/modules/home/data/models/home_page_model';
+import Meta from '../src/shared/components/Meta';
+import { ErrorModel } from '../src/shared/errors/error_model';
+import { BiErrorAlt } from 'react-icons/bi';
+import AboutUs from '../src/modules/home/components/AboutUs';
+import Achievements from '../src/modules/home/components/Achievements';
+import { useAuthContext } from '../src/modules/auth/AuthContext';
 
 interface HomeProps {
   error: ErrorModel | null;
@@ -27,15 +27,15 @@ const Home: NextPage<HomeProps> = ({ error, results }) => {
 
   return (
     <div>
-      <Meta titlePrefix="Home" />
+      <Meta titlePrefix='Home' />
 
       {!data ? (
-        <div className="flex flex-col items-center justify-center h-screen">
-          <BiErrorAlt className="mb-8 text-9xl text-error" />
-          <h2 className="heading2">{error?.name ?? "An error occured"}</h2>
+        <div className='flex flex-col items-center justify-center h-screen'>
+          <BiErrorAlt className='mb-8 text-9xl text-error' />
+          <h2 className='heading2'>{error?.name ?? 'An error occured'}</h2>
           <p>
             {error?.message ??
-              "Consider checking your internet connection and then refreshing this page"}
+              'Consider checking your internet connection and then refreshing this page'}
           </p>
         </div>
       ) : (
